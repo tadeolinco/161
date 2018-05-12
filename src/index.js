0c60ce12200d218a8cfc450c25d52597e6ff0c71
Zoe Villanueva <zoektv@gmail.com>
2018-05-13 00:37:34 +0800

    Add camera restrictions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-/* 
-  From example:
-  https://doc.babylonjs.com/babylon101/first
-*/
-
 import {
   Engine,
   HemisphericLight,
   MeshBuilder,
   PointLight,
   Scene,
-  Vector3,
   UniversalCamera,
+  Vector3,
 } from 'babylonjs'
 
 const canvas = document.getElementById('canvas')
@@ -18,28 +13,40 @@ const canvas = document.getElementById('canvas')
 const engine = new Engine(canvas, true)
 engine.isPointerLock = true
 
-const createScene = function() {
-  // Create the scene space
-  const scene = new Scene(engine)
+// Create the scene space
+const scene = new Scene(engine)
 
-  // Add a camera to the scene and attach it to the canvas
-  const camera = new UniversalCamera('Camera', new Vector3(0, 0, -10), scene)
-  camera.angularSensibility = 5000
-  camera.setTarget(Vector3.Zero())
-  camera.attachControl(canvas, true)
+// Add a camera to the scene and attach it to the canvas
+const camera = new UniversalCamera('Camera', new Vector3(0, 0, -10), scene)
+camera.angularSensibility = 5000
+camera.setTarget(Vector3.Zero())
+camera.attachControl(canvas, true)
 
-  // Add lights to the scene
-  const light1 = new HemisphericLight('light1', new Vector3(1, 1, 0), scene)
-  const light2 = new PointLight('light2', new Vector3(0, 1, -1), scene)
+// Add lights to the scene
+const light1 = new HemisphericLight('light1', new Vector3(1, 1, 0), scene)
+const light2 = new PointLight('light2', new Vector3(0, 1, -1), scene)
 
-  // Add and manipulate meshes in the scene
-  const sphere = MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene)
-
-  return scene
-}
-
-const scene = createScene()
+// Add and manipulate meshes in the scene
+const sphere = MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene)
 
 engine.runRenderLoop(() => {
+  /**
+   * Restricts camera movement
+   * Adjust the denominators for Math.PI to adjust the restriction
+   */
+  const camera = scene.cameras[0]
+  const { x, y } = camera.rotation
+  if (y > Math.PI / 8) {
+    camera.rotation.y = Math.PI / 8
+  } else if (y < -Math.PI / 8) {
+    camera.rotation.y = -Math.PI / 8
+  }
+
+  if (x > Math.PI / 16) {
+    camera.rotation.x = Math.PI / 16
+  } else if (x < -Math.PI / 16) {
+    camera.rotation.x = -Math.PI / 16
+  }
+
   scene.render()
 })
