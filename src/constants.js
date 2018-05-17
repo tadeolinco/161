8f6d2ba950cc839225ebc50f8c7d5e78e12937c8
Sam Bautista <tadeolinco@gmail.com>
2018-05-17 22:29:32 +0800

    Adjust gun position

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ import { Vector3 } from 'babylonjs'
 
 export const BASE_UNIT = 3 // Base unit for walls and lights
 
-export const GUN_POSITION = new Vector3(0, -0.5, 1.5)
+export const GUN_POSITION = new Vector3(0, -0.5, 1.6)
 
 export const BULLET_SPEED = 1
 
